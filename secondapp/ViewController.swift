b180ebe09257dc18aacf0fee2a2738e0c087995e
kojima-norihiko <norihiko.kojima@nex.nikkei.com>
2019-12-20 17:13:44 +0900

    Initial Commit

---
+++ secondapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondapp
+//
+//  Created by kojima on 2019/12/20.
+//  Copyright © 2019 kojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
